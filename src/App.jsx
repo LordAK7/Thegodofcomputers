6139d94022f33019422a59ecba2dee73feb39150
Aditya Kamble <adityatinkercad@gmail.com>
2024-06-10 02:31:54 +0530

    Import statements changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
+import Navbar from "./components/Navbar";
+import Home from "./components/Hero";
+// import Timeline from "./components/Experience";
+import About from "./components/About";
+import Skills from "./components/Skills";
+// import More from "./components/More";
 
 export default function App() {
   return (
     <div>
-hello world
+      <Navbar/>
+      <Home/>
+      <About/>
+      <Skills/>
+      {/* <More/> */}
+      {/* <Timeline/> */}
     </div>
   );
 }
 
 
+
+ 
