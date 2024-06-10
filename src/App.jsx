dfa11f9c45cd378147f523aae7ca9877d4688ae4
Aditya Kamble <adityatinkercad@gmail.com>
2024-06-11 01:12:11 +0530

    fixed and updated the imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
 import Navbar from "./components/Navbar";
 import Home from "./components/Hero";
-// import Timeline from "./components/Experience";
-import About from "./components/About";
-import Skills from "./components/Skills";
-// import More from "./components/More";
+import Web from "./components/Web";
+import WebDevSkills from "./components/webSkills";
+import Dev from "./components/Dev";
+import DevSkills from "./components/devSkills";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+
 
 export default function App() {
   return (
     <div>
       <Navbar/>
       <Home/>
-      <About/>
-      <Skills/>
-      {/* <More/> */}
-      {/* <Timeline/> */}
+      <Web/>
+      <WebDevSkills/>
+      <Dev/>
+      <DevSkills/>
+      <Contact/>
+      <Footer/>
     </div>
   );
 }
