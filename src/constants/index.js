88ba69d6def4aabc563d7ac626ffece7b90ead38
Aditya Kamble <adityatinkercad@gmail.com>
2024-06-11 01:49:31 +0530

    cleaned shit up

---
+++ src/constants/index.js
@@ -0,0 +1,8 @@
+export const navigationLinks = [
+  { name: 'Home', href: '#home' },
+  { name: 'About', href: '#about' },
+  { name: 'Skills', href: '#skills' },
+  { name: 'Projects', href: '#projects' },
+  { name: 'Academics', href: '#academics' },
+  { name: 'Contact', href: '#contact' },
+];
