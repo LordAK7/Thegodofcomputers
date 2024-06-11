e932188a542e906cdad342a382522da41ac56c90
Aditya Kamble <adityatinkercad@gmail.com>
2024-06-11 15:31:33 +0530

    fixed the devSkills component for mobile devices

--- src/components/devSkills.jsx
+++ src/components/devSkills.jsx
@@ -10,6 +10,7 @@ import kerasSVG from '/dev/keras.svg';
 import javaSVG from '/dev/java.svg';
 import kubernetesSVG from '/dev/kubernetes.svg';
 import opencvSVG from '/dev/opencv.svg';
+import './devSkills.css';
 
 const SkillBadge = ({ svg, name, className, size }) => (
   <motion.div
@@ -28,22 +29,20 @@ const SkillBadge = ({ svg, name, className, size }) => (
 );
 
 const DevSkills = () => {
-  const logoSize = '63.99px'; // You can change this value to increase/decrease the size of all logos
-
   return (
     <>
       <div className="flex items-center justify-center bg-[#000005]">
-        <div className="mb-12 flex w-full flex-wrap items-center justify-center gap-10 px-5 sm:mb-24 sm:w-4/5 sm:px-0 lg:gap-16 xl:w-3/5">
-          <SkillBadge svg={pythonSVG} name="Python" size={logoSize} />
-          <SkillBadge svg={numpySVG} name="NumPy" size={logoSize} />
-          <SkillBadge svg={tensorflowSVG} name="TensorFlow" size={logoSize} />
-          <SkillBadge svg={matplotlibSVG} name="Matplotlib" size={logoSize} />
-          <SkillBadge svg={kerasSVG} name="Keras" size={logoSize} />
-          <SkillBadge svg={pytorchSVG} name="PyTorch" size={logoSize} />
-          <SkillBadge svg={pandasSVG} name="Pandas" size={logoSize} />
-          <SkillBadge svg={opencvSVG} name="OpenCV" size={logoSize} />
-          <SkillBadge svg={kubernetesSVG} name="Kubernetes" size={logoSize} />
-          <SkillBadge svg={javaSVG} name="java" size={logoSize} />
+        <div className="mb-12 flex w-full flex-wrap items-center justify-center gap-5 sm:gap-10 px-5 sm:mb-24 sm:w-4/5 sm:px-0 lg:gap-16 xl:w-3/5">
+          <SkillBadge svg={pythonSVG} name="Python" className="logo-size" />
+          <SkillBadge svg={numpySVG} name="NumPy" className="logo-size" />
+          <SkillBadge svg={tensorflowSVG} name="TensorFlow" className="logo-size" />
+          <SkillBadge svg={matplotlibSVG} name="Matplotlib" className="logo-size" />
+          <SkillBadge svg={kerasSVG} name="Keras" className="logo-size" />
+          <SkillBadge svg={pytorchSVG} name="PyTorch" className="logo-size" />
+          <SkillBadge svg={kubernetesSVG} name="Kubernetes" className="logo-size" />
+          <SkillBadge svg={pandasSVG} name="Pandas" className="logo-size" />
+          <SkillBadge svg={opencvSVG} name="OpenCV" className="logo-size" />
+          <SkillBadge svg={javaSVG} name="Java" className="logo-size" />
         </div>
       </div>
       <div className="bg-[#000005] h-32"> {/* Adjust the height as needed */}
